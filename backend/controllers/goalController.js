d08ff0ddf13dbec827faffca9206421a2200c999
zakaria <zakariasoukarieh@gmail.com>
2023-02-27 00:15:18 +0200

    adding-modles

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -11,21 +11,38 @@ const getGoals = asyncHandler(async (req, res) => {
 	res.status(200).json(goals);
 });
 
-// @desc    Set goal
-// @route   POST /api/goals
-// @access  Private
+
 const setGoal = asyncHandler(async (req, res) => {
-	if (!req.body.text) {
+	if (!req.body.text || !req.body.phone || !req.body.age) {
 		res.status(400);
-		throw new Error("Please add a text field");
+		throw new Error("Please fill all required fields");
 	}
 
 	const goal = await Goal.create({
 		text: req.body.text,
+		phone: req.body.phone,
+		age: req.body.age,
 	});
 
 	res.status(200).json(goal);
 });
+
+
+// @desc    Set goal
+// @route   POST /api/goals
+// @access  Private
+// const setGoal = asyncHandler(async (req, res) => {
+// 	if (!req.body.text) {
+// 		res.status(400);
+// 		throw new Error("Please add a text field");
+// 	}
+
+// 	const goal = await Goal.create({
+// 		text: req.body.text,
+// 	});
+
+// 	res.status(200).json(goal);
+// });
 
 // @desc    Update goal
 // @route   PUT /api/goals/:id
--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -6,6 +6,16 @@ const goalSchema = mongoose.Schema(
 			type: String,
 			required: [true, "Please add a text value"],
 		},
+		phone: {
+			type: String,
+			required: [true, "Please add a phone number"],
+		},
+		age: {
+			type: Number,
+			required: [true, "Please add an age"],
+			min: [0, "Age must be a positive number"],
+			max: [120, "Age must be less than or equal to 120"],
+		},
 	},
 	{
 		timestamps: true,
